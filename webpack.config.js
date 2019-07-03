4993207422dde0a1a87cbd3c294101feceac2fcd
githubxingxing520 <121507664@qq.com>
2019-07-03 16:33:47 +0800

    project init

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry:"./src/main.js",
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+      },
+      plugins:[
+          new HtmlWebpackPlugin({template:path.join(__dirname,"./src/index.html")})
+      ],
+      module:{
+        rules: [
+            { test: /\.css$/, use: ["style-loader","css-loader"] },
+            { test: /\.less$/, use: ["style-loader","css-loader","less-loader"] },
+            { test: /\.scss$/, use: ["style-loader","css-loader","sass-loader"] },
+            { test: /\.(jpg|jpeg|gif|png|bmp)$/, use: "url-loader?limit=5000&name=[hash:8]-[name].[ext]" },
+            { test: /\.(ttf|eot|svg|woff|woff2)$/, use: "url-loader" },
+            { test: /\.js$/, use: "babel-loader",exclude:/node_modules/ },
+            { test: /\.vue$/, use: "vue-loader" }
+          ]
+      }
+}
